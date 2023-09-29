c6b58c9ec5492aa2fb2f3601046a69f119e2489c
Tarsis <br.tmvdl@gmail.com>
2023-09-28 21:07:18 -0300

    Adding notebooks files

---
+++ src/notebooks/from-getupdates-to-getchat.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/sh
+
+path="getUpdates"
+
+for update in $( ls "./data/${path}/" )
+
+do
+
+for chat_id in $( cat "data/getUpdates/${update}/resp" | jq ".result[].message.chat.id" )
+
+do
+
+. ./src/getters/getChat.sh "${chat_id}"
+
+done
+
+done
